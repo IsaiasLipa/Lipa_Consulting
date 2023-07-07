ff569f3f617a35556bc705cdfb625b70273ac5fd
IsaiasLipa <>
2023-07-06 23:00:29 -0400

    contact buttons

--- src/components/CardContact.js
+++ src/components/CardContact.js
@@ -1,7 +1,23 @@
 import React from "react";
 import { Card, Row, Col } from "react-bootstrap";
-
+import { Whatsapp } from "react-bootstrap-icons";
+import { Button } from "react-bootstrap";
 const Card_contact = (props) => {
+  const openWhatsapp = () => {
+    window.open("https://wa.me/16317204949", "_blank");
+  }
+  const openFacebook = () => {
+    window.open("https://www.facebook.com/LipaConsulting/", "_blank");
+  }
+  const buttonStyle = {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  };
+
+  const iconStyle = {
+    marginRight: "5px",
+  };
   return (
     <Card className="m-4">
       <Card.Body>
@@ -35,10 +51,36 @@ const Card_contact = (props) => {
                 </div>
               ) : (
                 <div>
-                  <p>Para programar una cita, puedes llamarnos al: (631) 720-4949</p>
-                  <p>También puedes enviarnos un correo electrónico a: lipaconsultingserv@gmail.com</p>
+                  <p>
+                    Para programar una cita, puedes llamarnos al: (631) 720-4949
+                  </p>
+                  <p>
+                    También puedes enviarnos un correo electrónico a:
+                    lipaconsultingserv@gmail.com
+                  </p>
                 </div>
               )}
+              <Button
+                variant="outline-success"
+                onClick={openWhatsapp}
+                style={buttonStyle}
+              >
+                <div style={iconStyle}>
+                  <Whatsapp />
+                </div>
+                {props.language ? <a>Text our Whatsapp</a> : <a>Contactanos en Whatsapp</a>}
+              </Button>
+              <br/>
+              <Button
+                variant="outline-primary"
+                onClick={openFacebook}
+                style={buttonStyle}
+              >
+                <div style={iconStyle}>
+                  <Whatsapp />
+                </div>
+                {props.language ? <a>See our Facebook</a> : <a>Ve nuestro Facebook</a>}
+              </Button>
             </Col>
           </Row>
         </Card.Text>
